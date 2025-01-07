d2b8dffd490454fa66394ac150f226b8b49b3d0a
PurviCnc <purvicnc@gmail.com>
2025-01-07 15:39:25 +0530

    Redesign blog

--- src/components/blogcomponents/BlogList.jsx
+++ src/components/blogcomponents/BlogList.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState, useRef } from 'react';
 import './BlogList.css';
 import './BlogPageTitle.css'
 import Link from 'next/link';
+import moment from 'moment';
+import Pagination from './Pagination';
 
 const BlogList = ({
   blog_heading,
@@ -16,7 +18,6 @@ const BlogList = ({
   useEffect(() => {
     setIsClient(true); // Set to true after the component mounts
   }, []);
-
 
 
   const handleSmoothScroll = () => {
@@ -38,12 +39,13 @@ const BlogList = ({
     if (loadingRef.current) return;
     setisLoadingk(true);
     loadingRef.current = true;
-    let url = `${BASE_URL}/wp-json/wp/v2/posts?per_page=9&page=${page}`;
+    let url = `${BASE_URL}/wp-json/wp/v2/posts?per_page=12&page=${page}`;
     if (category) {
       url += `&categories=${category}`;
     }
     fetch(url)
       .then((response) => {
+
         const total_posts = response.headers.get('X-Wp-Total');
         const total = response.headers.get('X-Wp-Totalpages');
         setTotalPosts(parseInt(total_posts));
@@ -51,39 +53,39 @@ const BlogList = ({
         return response.json();
       })
       .then((data) => {
-        const transformedData = [];
-        for (let i = 0; i < data.length; i += 3) {
-          const mainPost = {
-            id: data[i].id,
-            slug: data[i].slug,
-            categories_names: data[i].categories_names,
-            title: data[i].title.rendered,
-            image: data[i].featured_image_url || "/assets/images/blogImg1.png",
-            date: new Date(data[i].date).toLocaleDateString('en-GB'),
-            author: data[i].author_name || 'Codeandcore',
-            duration: data[i].human_time_diff,
-            link: data[i].link,
-            innerdata: [],
-          };
+        // const transformedData = [];
+        // for (let i = 0; i < data.length; i += 3) {
+        //   const mainPost = {
+        //     id: data[i].id,
+        //     slug: data[i].slug,
+        //     categories_names: data[i].categories_names,
+        //     title: data[i].title.rendered,
+        //     image: data[i].featured_image_url || "/assets/images/blogImg1.png",
+        //     date: new Date(data[i].date).toLocaleDateString('en-GB'),
+        //     author: data[i].author_name || 'Codeandcore',
+        //     duration: data[i].human_time_diff,
+        //     link: data[i].link,
+        //     innerdata: [],
+        //   };
 
-          for (let j = 1; j <= 2; j++) {
-            if (i + j < data.length) {
-              mainPost.innerdata.push({
-                id: data[i + j].id,
-                slug: data[i + j].slug,
-                categories_names: data[i + j].categories_names,
-                title: data[i + j].title.rendered,
-                image: data[i + j].featured_image_url || "/assets/images/blogImg1.png",
-                date: new Date(data[i + j].date).toLocaleDateString('en-GB'),
-                author: data[i + j].author_name || 'Codeandcore',
-                duration: data[i + j].human_time_diff,
-                link: data[i + j].link,
-              });
-            }
-          }
-          transformedData.push(mainPost);
-        }
-        setBlogsData((prevBlogs) => [...prevBlogs, ...transformedData]);
+        //   for (let j = 1; j <= 2; j++) {
+        //     if (i + j < data.length) {
+        //       mainPost.innerdata.push({
+        //         id: data[i + j].id,
+        //         slug: data[i + j].slug,
+        //         categories_names: data[i + j].categories_names,
+        //         title: data[i + j].title.rendered,
+        //         image: data[i + j].featured_image_url || "/assets/images/blogImg1.png",
+        //         date: new Date(data[i + j].date).toLocaleDateString('en-GB'),
+        //         author: data[i + j].author_name || 'Codeandcore',
+        //         duration: data[i + j].human_time_diff,
+        //         link: data[i + j].link,
+        //       });
+        //     }
+        //   }
+        //   transformedData.push(mainPost);
+        // }
+        setBlogsData(data);
         loadingRef.current = false;
       })
       .catch((error) => {
@@ -99,25 +101,25 @@ const BlogList = ({
     fetchBlogs();
   }, [BASE_URL]);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (blogsListRef.current) {
-        const servicesListBottom =
-          blogsListRef.current.getBoundingClientRect().bottom + 100;
-        const windowBottom = window.innerHeight;
-        if (
-          servicesListBottom <= windowBottom &&
-          !loadingRef.current &&
-          currentPage < totalPages
-        ) {
-          setCurrentPage((prevPage) => prevPage + 1);
-        }
-      }
-    };
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (blogsListRef.current) {
+  //       const servicesListBottom =
+  //         blogsListRef.current.getBoundingClientRect().bottom + 100;
+  //       const windowBottom = window.innerHeight;
+  //       if (
+  //         servicesListBottom <= windowBottom &&
+  //         !loadingRef.current &&
+  //         currentPage < totalPages
+  //       ) {
+  //         setCurrentPage((prevPage) => prevPage + 1);
+  //       }
+  //     }
+  //   };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [currentPage, totalPages]);
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => window.removeEventListener('scroll', handleScroll);
+  // }, [currentPage, totalPages]);
 
   useEffect(() => {
     fetchBlogs(currentCategory, currentPage);
@@ -129,7 +131,67 @@ const BlogList = ({
     setBlogsData([]); // Clear previous blogs
     fetchBlogs(category, 1);
   };
-  if (!isClient) return null; 
+  const handlePageChange = (page) => {
+    fetchBlogs(currentCategory, page);
+  };
+
+  const specialBlogContents = [
+    {
+      backgroundImage: "/assets/images/special-blog-bg1.png",
+      title: "Get In Touch with Us",
+      content: "We’re here to answer your questions and bring your ideas to life. Reach out today!",
+      fontColor: "#FFFFFF",
+      btn: {
+        type:"primary",
+        url: "/contactus",
+        label: "Contact Us",
+        backgroundColor: "",
+        fontColor: "#FFFFFF"
+      }
+    },
+    {
+      backgroundImage: "/assets/images/special-blog-bg2.png",
+      title: "Powering the future",
+      content: "Discover the cutting-edge technologies that drive our solutions.",
+      fontColor: "#424242",
+      btn: {
+        type:"secondary",
+        url: "/technologies",
+        label: "Our Technologies",
+        backgroundColor: "",
+        fontColor: "#000000"
+      }
+    },
+    {
+      backgroundImage: "/assets/images/special-blog-bg1.png",
+      title: "Innovation in Action",
+      content: "Explore the projects that showcase our passion for design and technology.",
+      fontColor: "#FFFFFF",
+      btn: {
+        type:"primary",
+        url: "/portfolio",
+        label: "Our Portfolio",
+        backgroundColor: "",
+        fontColor: "#FFFFFF"
+      }
+    },
+    {
+      backgroundImage: "/assets/images/special-blog-bg2.png",
+      title: "Talk to Us Today",
+      content: "Have a question or a project in mind? Let’s make your ideas happen!Let’s start a conversation!",
+      fontColor: "#424242",
+      btn: {
+        type:"secondary",
+        url: "/contactus",
+        label: "Contact Us",
+        backgroundColor: "",
+        fontColor: "#000000"
+      }
+    },
+  ];
+
+
+  if (!isClient) return null;
   return (
     <div className="blog_main" ref={blogsListRef}>
       <div className="blog_page_title">
@@ -177,103 +239,77 @@ const BlogList = ({
       </div>
       <div className="blog_section">
         <div className="wrapper">
-          {blogData.map((blog, index) => {
-            return (
-              <div key={blog.id} className="blog_row">
-                <div className="blog_list">
-                  <Link
-                    href={`/blog/${blog.slug}`}
-                    // onMouseEnter={() => handleMouseEnter(blog.slug)}
-                    className="blog_img"
-                  >
-                    <img src={blog.image} alt={blog.title} />
-                  </Link>
-                  <div className="blog-content">
-                    <div className="blog_info d_flex">
-                      <div className="col-left d_flex">
-                        <a href={blog.link} className="btnmix">
-                          <em
-                            dangerouslySetInnerHTML={{
-                              __html: blog.categories_names,
-                            }}
-                          ></em>
-                        </a>
-                        <span className="date">
-                          <img src={"/assets/images/dateIcon.svg"} alt="date_icon" /> {blog.date}
-                        </span>
-                      </div>
-                      <div className="col-right">
-                        <label className="d_flex">
-                          <img src={"../../assets/images/cnc-icon.svg"} alt="time_icon" /> By{' '}
-                          <a href="#">{blog.author}</a>
-                          <span>{blog.duration}</span>
-                        </label>
-                      </div>
-                    </div>
-                    <h3>
-                      <Link
-                        href={`/blog/${blog.slug}`}
-                        // onMouseEnter={() => handleMouseEnter(blog.slug)}
-                      >
-                        {/* {blog.title} */}
-                        {blog.title.replace(/<\/?br\s*\/?>/gi, ' ')}
-                      </Link>
-                    </h3>
+          <div className='blogItemList'>
+            {blogData.map((blog, index) => {
+              if ((index + 1) % 3 === 0) {
+                const specialIndex = (Math.floor(index / 3) % specialBlogContents.length);
+                const specialContent = specialBlogContents[specialIndex];
+                return (<div key={index} className="blog-item special-layout" style={{ backgroundImage: `url(${specialContent?.backgroundImage})` }}>
+                  {/* <Link href={`/blog/${blog.slug}`} className="blog_img">
+                    <img src={blog.featured_image_url} alt={blog?.title?.rendered} />
+                  </Link>*/}
+                  <div className="special-content">
+                    <h2 className="special-blog-title" style={{ color: specialContent?.fontColor }}>
+                      {specialContent.title}
+                    </h2>
+                    <p className="content" style={{ color: specialContent?.fontColor }}>{specialContent.content}</p>
+                    <Link
+                      href={specialContent?.btn.url}
+                      style={{
+                        // color: specialContent?.btn?.fontColor
+                        color:"#ffff"
+                      }}
+                      className={`btn  ${specialContent?.btn?.type==="primary" ? "" : "btn-secondary"}`}
+                     
+                    >
+                      <em>{specialContent?.btn.label}</em>
+                    </Link>
                   </div>
-                </div>
-                <div className="blog_list">
-                  {blog.innerdata.map((innerItem) => (
-                    <div key={innerItem.id} className="blog_small">
-                      <Link
-                        href={`/blog/${innerItem.slug}`}
-                        // onMouseEnter={() => handleMouseEnter(innerItem.slug)}
-                        className="blog_img"
-                      >
-                        <img src={innerItem.image} alt={blog.title} />
-                      </Link>
-                      <div className="blog-content">
-                        <div className="blog_info d_flex">
-                          <div className="col-left d_flex">
-                            <a href={innerItem.link} className="btnmix">
-                              <em
-                                dangerouslySetInnerHTML={{
-                                  __html: innerItem.categories_names,
-                                }}
-                              ></em>
-                            </a>
-                            <span className="date">
-                              <img src={"/assets/images/dateIcon.svg"} alt="date_icon" />{' '}
-                              {innerItem.date}
-                            </span>
+                </div>)
+              } else {
+                return (
+                  <div
+                    key={index}
+                    className={`blog-item`}
+                  >
+                    <Link
+                      href={`/blog/${blog.slug}`}
+                      className="blog_img"
+                    >
+                      <img src={blog.featured_image_url} alt={blog?.title?.rendered} />
+                    </Link>
+                    <div className="blog-content">
+                      <div className="blog_info d_flex">
+                        <div className="col-left d_flex">
+                          <a href={blog.link}>
+                            <span
+                              dangerouslySetInnerHTML={{
+                                __html: blog.categories_names,
+                              }}
+                            ></span>
+                          </a>
+                          <div className="date">
+                            <img src={"/assets/images/dateIcon.svg"} alt="date_icon" />{moment(blog.date).format('D.M.YYYY')}
                           </div>
                         </div>
-                        <h3>
-                          <Link
-                            href={`/blog/${innerItem.slug}`}
-                            // onMouseEnter={() => handleMouseEnter(innerItem.slug)}
-                          >
-                            {innerItem.title}
-                          </Link>
-                        </h3>
-                        <div className="col-right">
-                          <label className="d_flex">
-                            <img src={"/assets/images/cnc-icon.svg"} alt="time_icon" /> By{' '}
-                            <Link
-                              href={`/blog/${innerItem.slug}`}
-                            >
-                              {innerItem.author}
-                            </Link>
-                            <span>{innerItem.duration}</span>
-                          </label>
-                        </div>
                       </div>
+                      <h3>
+                        <Link
+                          href={`/blog/${blog.slug}`}
+                        >
+                          {blog.title?.rendered?.replace(/<\/?br\s*\/?>/gi, ' ')}
+                        </Link>
+                      </h3>
                     </div>
-                  ))}
-                </div>
-              </div>
-            );
-          })}
+                  </div>
+                )
+              }
+            })}
+          </div>
         </div>
+      </div>
+      <div className='pagination-section'>
+        <Pagination totalPages={totalPages} onPageChange={handlePageChange} currentPage={currentPage} setCurrentPage={setCurrentPage} />
       </div>
     </div>
   );
--- src/components/blogcomponents/Pagination.jsx
+++ src/components/blogcomponents/Pagination.jsx
@@ -1,45 +1,53 @@
-import React from 'react';
-import './Pagination.css';
-const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-  const pageNumbers = [];
-  for (let i = 1; i <= totalPages; i++) {
-    pageNumbers.push(i);
-  }
+import React, { useState } from "react";
+import "./Pagination.css";
+
+const Pagination = ({ totalPages, onPageChange,currentPage,setCurrentPage }) => {
+
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      const newPage = currentPage - 1;
+      setCurrentPage(newPage);
+      onPageChange(newPage);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      const newPage = currentPage + 1;
+      setCurrentPage(newPage);
+      onPageChange(newPage);
+    }
+  };
+
+  const handlePageClick = (page) => {
+    setCurrentPage(page);
+    onPageChange(page);
+  };
+
   return (
-    <div className="wrapper">
-      <nav>
-        <ul className="pagination d_flex">
-          <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-            <a
-              className="page-link"
-              onClick={() => onPageChange(currentPage - 1)}
+    <div className="pagination-container">
+      <button className="arrow-button" onClick={handlePrevious} disabled={currentPage === 1}>
+        &#x276E;
+      </button>
+      <div className="pagination-numbers">
+        {Array.from({ length: totalPages }, (_, index) => {
+          const page = index + 1;
+          return (
+            <div
+              key={page}
+              className={`page-number ${page === currentPage ? "active" : ""}`}
+              onClick={() => handlePageClick(page)}
             >
-              Prev
-            </a>
-          </li>
-          {pageNumbers.map((number) => (
-            <li
-              key={number}
-              className={`page-item ${currentPage === number ? 'active' : ''}`}
-            >
-              <a className="page-link" onClick={() => onPageChange(number)}>
-                {number}
-              </a>
-            </li>
-          ))}
-          <li
-            className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}
-          >
-            <a
-              className="page-link"
-              onClick={() => onPageChange(currentPage + 1)}
-            >
-              Next
-            </a>
-          </li>
-        </ul>
-      </nav>
+              {page}
+            </div>
+          );
+        })}
+      </div>
+      <button className="arrow-button" onClick={handleNext} disabled={currentPage === totalPages}>
+        &#x276F;
+      </button>
     </div>
   );
 };
+
 export default Pagination;
