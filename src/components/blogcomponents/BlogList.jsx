0f3492d92ac163706751900ec92c9dc5e378f26f
ABHINAV <codeandcore@gmail.com>
2025-01-21 20:02:32 +0530

    fixes as per mayur sir

--- src/components/blogcomponents/BlogList.jsx
+++ src/components/blogcomponents/BlogList.jsx
@@ -1,10 +1,12 @@
 'use client'
 import React, { useEffect, useState, useRef } from 'react';
 import './BlogList.css';
-import './BlogPageTitle.css'
+import './BlogPageTitle.css';
 import Link from 'next/link';
 import moment from 'moment';
 import Pagination from './Pagination';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'; // Import skeleton styles
 import { ReplaceDomain } from '@/ReplaceDomain';
 
 const BlogList = ({
@@ -16,49 +18,42 @@ const BlogList = ({
 }) => {
   const [isLoadingk, setisLoadingk] = useState(true);
   const [isClient, setIsClient] = useState(false);
-  useEffect(() => {
-    setIsClient(true); // Set to true after the component mounts
-  }, []);
-
-
-  const handleSmoothScroll = () => {
-    window.scrollTo({
-      top: 0, // Adjust the value as needed
-      behavior: 'auto',
-    });
-  };
-
   const [blogData, setBlogsData] = useState([]);
   const [currentCategory, setCurrentCategory] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPosts, setTotalPosts] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
-  const [specialBlogElements,setSpecialBlogElements]=useState([])
+  const [specialBlogElements, setSpecialBlogElements] = useState([]);
   const loadingRef = useRef(false);
   const blogsListRef = useRef(null);
   const [specialIndexOffset, setSpecialIndexOffset] = useState(0);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   const fetchBlogs = async (category = null, page = 1) => {
     if (loadingRef.current) return;
     setisLoadingk(true);
     loadingRef.current = true;
-  
+
     try {
       let url = `${BASE_URL}/wp-json/wp/v2/posts?per_page=12&page=${page}`;
       if (category) {
         url += `&categories=${category}`;
       }
-  
+
       const response = await fetch(url);
-  
+
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-  
+
       const total_posts = response.headers.get('X-Wp-Total');
       const total = response.headers.get('X-Wp-Totalpages');
       setTotalPosts(parseInt(total_posts));
       setTotalPages(parseInt(total));
-  
+
       const data = await response.json();
       setBlogsData(data);
     } catch (error) {
@@ -70,21 +65,20 @@ const BlogList = ({
   };
 
   const fetchspecialBlogs = () => {
-    fetch('https://wordpress-1074629-4621962.cloudwaysapps.com/wp-json/options/all',  { cache: "no-store" } )
-    .then(response => response.json())
-      .then(data => {
-        setSpecialBlogElements(data?.advertise_repeater)
-    })
-  }
+    fetch('https://wordpress-1074629-4621962.cloudwaysapps.com/wp-json/options/all', { cache: 'no-store' })
+      .then((response) => response.json())
+      .then((data) => {
+        setSpecialBlogElements(data?.advertise_repeater);
+      });
+  };
 
   useEffect(() => {
     fetchBlogs();
-   
   }, [BASE_URL]);
 
   useEffect(() => {
-    fetchspecialBlogs()
-  },[])
+    fetchspecialBlogs();
+  }, []);
 
   useEffect(() => {
     fetchBlogs(currentCategory, currentPage);
@@ -92,27 +86,19 @@ const BlogList = ({
 
   const handleCategoryChange = (category) => {
     setCurrentCategory(category);
-    setCurrentPage(1); // Reset to first page when category changes
-    setBlogsData([]); // Clear previous blogs
+    setCurrentPage(1);
+    setBlogsData([]);
     fetchBlogs(category, 1);
   };
+
   const handlePageChange = (page) => {
-    handleSmoothScroll()
-    setBlogsData([]); 
+    window.scrollTo({ top: 0, behavior: 'auto' });
+    setBlogsData([]);
     fetchBlogs(currentCategory, page);
   };
 
-
-
-  useEffect(() => {
-    // Calculate the total number of special items in the current blogData
-    if (Array.isArray(blogData) && blogData.length > 0) {
-      const pageOffset = ((currentPage - 1) % 2) * 2
-      setSpecialIndexOffset(pageOffset);
-    }
-  }, [blogData,currentPage]);
-
   if (!isClient) return null;
+
   return (
     <div className="blog_main" ref={blogsListRef}>
       <div className="blog_page_title">
@@ -149,7 +135,7 @@ const BlogList = ({
           )}
         </div>
       </div>
-      <div
+      {/* <div
         className="loader_blog"
         style={{ display: isLoadingk ? 'block' : 'none' }}
       >
@@ -157,74 +143,61 @@ const BlogList = ({
           src={'../../assets/images/rotate-right.png'}
           alt="rotate-right"
         />
-      </div>
+      </div> */}
       <div className="blog_section">
         <div className="wrapper">
-          <div className='blogItemList'>
-            {blogData.map((blog, index) => {
-              if ((index + 1) % 6 === 0) {
-                const specialIndex = (specialIndexOffset + Math.floor(index / 6)) % specialBlogElements.length;
-                const specialContent = specialBlogElements[specialIndex];                
-                return (<div key={index} className="blog-item special-layout" style={{ backgroundImage: `url(${specialContent?.background_image?.url})` }}>             
-                  <div className="special-content">
-                    <h3 className="special-blog-title" style={{ color:specialContent?.button_type==="Primary" ? "#ffff" :"#424242"}}>
-                      {specialContent?.title}
-                    </h3>
-                    <p className="content" style={{ color:specialContent?.button_type==="Primary" ? "#ffff" :"#424242"}}>{specialContent?.subtitle
-                    }</p>
-                    <Link
-                      href={specialContent?.button?.url || ""}
-                      style={{ color: specialContent?.button_type === "Primary" ? "#ffff" : "#424242" }}
-                      className={`btn  ${specialContent?.button_type==="Primary" ? "" : "btn-secondary"}`}
-                    >
-                      <em>{specialContent?.button?.title}</em>
-                    </Link>
-                  </div>
-                </div>)
-              } else {
-                return (
-                  <div
-                    key={index}
-                    className={`blog-item`}
-                  >
-                    <Link
-                      href={`/blog/${blog.slug}` || ""}
-                      className="blog_img"
-                    >
-                      <img src={blog.featured_image_url} alt={blog?.title?.rendered} />
-                    </Link>
-                    <div className="blog-content">
-                      <div className="blog_info d_flex">
-                        <div className="col-left d_flex">
-                          <a role="button">
-                            <span
-                              dangerouslySetInnerHTML={{
-                                __html: blog.categories_names,
-                              }}
-                            ></span>
-                          </a>
-                          <div className="date">
-                            <img src={"/assets/images/dateIcon.svg"} alt="date_icon" />{moment(blog.date).format('D.M.YYYY')}
-                          </div>
+          {isLoadingk ? (
+            <div className="skeleton-grid moving-background">
+              {Array.from({ length: 6 }).map((_, index) => (
+                <div key={index} className="skeleton-item">
+                  <Skeleton height={300} width="100%" style={{ marginBottom: 30 }} />
+                  {/* <Skeleton height={30} width="60%" style={{ marginTop: 10 }} /> */}
+                  {/* <Skeleton height={20} width="80%" /> */}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="blogItemList">
+              {blogData.map((blog, index) => (
+                <div key={index} className="blog-item">
+                  <Link href={`/blog/${blog.slug}`} className="blog_img">
+                    <img src={blog.featured_image_url} alt={blog?.title?.rendered} />
+                  </Link>
+                  <div className="blog-content">
+                    <div className="blog_info d_flex">
+                      <div className="col-left d_flex">
+                        <a role="button">
+                          <span
+                            dangerouslySetInnerHTML={{
+                              __html: blog.categories_names,
+                            }}
+                          ></span>
+                        </a>
+                        <div className="date">
+                          <img src="/assets/images/dateIcon.svg" alt="date_icon" />
+                          {moment(blog.date).format('D.M.YYYY')}
                         </div>
                       </div>
-                      <h3>
-                        <Link
-                          href={`/blog/${blog.slug}`}
-                        >
-                          {blog.title?.rendered?.replace(/<\/?br\s*\/?>/gi, ' ')}
-                        </Link>
-                      </h3>
                     </div>
+                    <h3>
+                      <Link href={`/blog/${blog.slug}`}>
+                        {blog.title?.rendered?.replace(/<\/?br\s*\/?>/gi, ' ')}
+                      </Link>
+                    </h3>
                   </div>
-                )
-              }
-            })}
-          </div>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
-      <div className='pagination-section'>
-        <Pagination totalPages={totalPages} onPageChange={handlePageChange} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <div className="pagination-section">
+        <Pagination
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
       </div>
     </div>
   );
