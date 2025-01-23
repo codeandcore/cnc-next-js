ba375a06ce6a388d95cf48458d334624ac756ca8
PurviCnc <purvicnc@gmail.com>
2025-01-23 12:02:00 +0530

    added Video on review page

--- src/components/Reviewcomponents/ReviewBanner.jsx
+++ src/components/Reviewcomponents/ReviewBanner.jsx
@@ -1,14 +1,20 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
+import dynamic from 'next/dynamic';
 import './ReviewBanner.css';
 import Link from 'next/link';
+import FsLightbox from 'fslightbox-react';
 
-const ReviewBanner = ({
+const ReviewBanner = React.memo(({
   banner_title,
   banner_description,
   right_side_title,
   discover_more,
 }) => {
+
+  const [isLightboxOpen, setLightboxOpen] = useState(false);
+  const [videoKey, setVideoKey] = useState(Date.now());
+  const [modalVideoKey, setModalVideoKey] = useState();
 const handleSmoothScroll = (e) => {
 e.preventDefault()
     const jobOpeningsSection = document.querySelector('.review_contant');    
@@ -20,6 +26,16 @@ e.preventDefault()
     }
   };
 
+  const handlePlayClick = () => {
+    setModalVideoKey(Date.now())
+    setLightboxOpen(!isLightboxOpen);
+  };
+
+  const handleVideoPlay = () => {
+    console.log('Video is playing');
+  };
+  
+
   return (
     <div className="review_banner">
       <div className="wrapper ">
@@ -29,12 +45,48 @@ e.preventDefault()
             {banner_description && (
               <p dangerouslySetInnerHTML={{ __html: banner_description }}></p>
             )}
+            <div className='all_reviews d_flex'>
             <Link className="see_all" href={""} onClick={(e)=>handleSmoothScroll(e)}>
               <em>
                 Read <br /> All Reviews
               </em>{' '}
               <img src={"/assets/images/dd_arrow.svg"} />
             </Link>
+            <div className='video_area'>
+            <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            preload="metadata"
+            className="video"
+            >
+              <source src={"/assets/video/reviewVideo.mp4"} type="video/mp4" />
+            </video>
+            <div className="play_button" onClick={handlePlayClick}>
+                  PLAY
+                </div>
+              </div>
+            </div>
+              <FsLightbox
+              toggler={isLightboxOpen}
+              types={["video"]}
+                sources={[
+                  "/assets/video/reviewVideo.mp4?autoplay=1&muted=1&loop=1",
+                ]}
+                options={{
+                  slide: {
+                    video: {
+                      key:modalVideoKey,
+                      width: "100vw",  // Full width
+                      height: "90vh",
+                      autoplay: 1,
+                      mute: 1,
+                      loop:true                 
+                    },
+                  },
+                }}
+              />
           </div>
           <div className="col_right">
             {right_side_title && <h3>{right_side_title}</h3>}
@@ -68,6 +120,6 @@ e.preventDefault()
       </div>
     </div>
   );
-};
+})
 
 export default ReviewBanner;
