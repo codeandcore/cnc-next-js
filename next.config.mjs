0676f27945a944fa4fb0a7f0f1783cd335bac6fe
ABHINAV <codeandcore@gmail.com>
2025-01-27 15:14:32 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 import path from 'path';
+
 const nextConfig = {
   reactStrictMode: false,
 
@@ -8,18 +9,16 @@ const nextConfig = {
       'https://cnc-website-new.vercel.app',
     ],
   },
-  webpack: (config, {
-    buildId,
-    webpack
-}) => {
 
+  webpack: (config, { buildId, webpack }) => {
     config.plugins.push(
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            "window.jQuery": "jquery"
-        })
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+      })
     );
+
     config.module.rules.push({
       test: /\.(mp4|webm|ogg)$/,
       use: {
@@ -30,15 +29,35 @@ const nextConfig = {
         },
       },
     });
+
     config.module.rules.push({
       test: /\.node/,
       use: "raw-loader",
     });
+
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       canvas: false,
     };
+
     return config;
+  },
+
+  async redirects() {
+    const res = await fetch(
+      process.env.NEXT_PUBLIC_REACT_APP_ENV !== "development"
+      ? `${process.env.NEXT_PUBLIC_VERCEL_URL}data/redirect/yoast`
+      : `${process.env.NEXT_PUBLIC_WP_URL}wp-json/custom/v1/yoast_redirects`);
+    const data = await res.json();
+
+    const redirects = data.yoast_seo_redirects.map((redirect) => ({
+      source: `/${redirect.old_url}`,
+      destination: `/${redirect.new_url}`,
+      permanent: false,
+      statusCode: 301
+    }));
+
+    return redirects;
   },
 };
 
